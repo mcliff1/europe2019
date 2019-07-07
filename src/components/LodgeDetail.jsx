a1a825745f1d0b75ceb53f181ef29222ea684692
Matt Cliff <matt@denvercliffs.com>
2019-07-07 11:42:53 -0600

    updates to lodging

--- src/components/LodgeDetail.jsx
+++ src/components/LodgeDetail.jsx
@@ -8,8 +8,9 @@ class LodgeDetail extends Component {
       <div className="p-3">
         <table className="Table" border="1">
           <tbody>
-            <tr><td colspan="2">Name:r {info.name}</td></tr>
-            <tr><td>Address: {info.address}</td><td>1</td></tr>
+            <tr><td colspan="2"><a href={info.url}>{info.name}</a></td><td>{info.arrive} - {info.depart}</td></tr>
+            <tr><td colspan="2">{info.address}</td><td>{info.phone}</td></tr>
+            <tr><td colspan="2">{info.note}</td><td>in:{info.checkin}, out: {info.checkout}</td></tr>
           </tbody>
         </table>
       </div>
--- src/components/LodgeInfo.jsx
+++ src/components/LodgeInfo.jsx
@@ -3,20 +3,71 @@ import LodgeDetail from './LodgeDetail';
 
 const lodgeInfo = [
   {
-    name: "Denver",
-    address: "Rejevik",
-    departure: "20 Jul 5:12P",
-    arrival: "20 Jul 6:50P",
+    name: "Cocoon Hauptbahnof",
+    address: "Mittererstraße 9, 80336 München",
+    arrive: "13 Jul",
+    depart: "16 Jul",
+    phone: "+49 89 548018 99 05",
+    maps: "https://www.google.de/maps/place/Hotel+Cocoon+Hauptbahnhof/@48.137959,11.5554048,17z/data=!3m1!4b1!4m9!3m8!1s0x479e75f8ec7a8db3:0x1183cb78b077c535!5m3!1s2018-08-12!4m1!1i2!8m2!3d48.137959!4d11.5575934",
+    email: "hauptbahnhof@cocoon-hotels.de",
+    checkout: "2P",
+    checkin: "11A",
+    url: "https://cocoon-hotels.de/en/cocoon-hauptbahnhof.html",
+    note: ""
+  },
+  {
+    name: "Imlauer Hotel Pitter",
+    address: "Rainestr.6, Salzburg",
+    phone: "+43 662 88978807",
+    arrive: "16 Jul",
+    depart: "17 Jul",
+    checkin: "3P",
+    checkout: "11A",
+    url: "https://imlauer.com/hotel-pitter-salzburg/",
+    note: ""
+  },
+  {
+    name: "Vienna Prestige - Opal Suite 17",
+    address: "Graben 27-28 1010 Wien",
+    phone: " +43 1 996 30 10",
+    email: "reception@welcome2vienna.com",
+    arrive: "17 Jul",
+    depart: "20 Jul",
+    checkin: "2P",
+    checkout: "11A",
+    url: "",
+    note: "no checkin counter, private apartment"
+  },
+  {
+    name: "Rome - Stronio",
+    address: "Via dell'Esquilino 38",
+    phone: "+39 349 644 0888",
+    arrive: "20 Jul",
+    depart: "24 Jul",
+    checkin: "3P",
+    checkout: "11A",
+    url: "https://www.vrbo.com/8086018ha",
+    note: "owner: Guido"
+  },
+  {
+    name: "Florence",
+    address: "",
+    arrive: "24 Jul",
+    depart: "26 Jul",
+    checkin: "2P",
+    checkout: "11A",
     url: "",
     note: ""
   },
   {
-    name: "Vienna",
-    address: "Rome`",
-    departure: "20 Jul 5:12P",
-    arrival: "20 Jul 6:50P",
+    name: "Munich",
+    address: "",
+    arrive: "17 Jul",
+    depart: "20 Jul",
+    checkin: "2P",
+    checkout: "11A",
     url: "",
-    note: ""
+    note: "no checkin counter, private apartment"
   }
 ]
 
