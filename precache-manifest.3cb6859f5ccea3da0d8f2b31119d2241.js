7509af27e1a7038cf52d28f9f8213100a898a102
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-03-13 04:02:51 +0000

    Deploy mcliff1/europe to github.com/mcliff1/europe.git:gh-pages

---
+++ precache-manifest.3cb6859f5ccea3da0d8f2b31119d2241.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "775d691aea161ac19971a9cc88ec4452",
+    "url": "/europe/index.html"
+  },
+  {
+    "revision": "d6a9a2c91322d798b8f3",
+    "url": "/europe/static/css/main.1779617f.chunk.css"
+  },
+  {
+    "revision": "4ebb59ca172250aec334",
+    "url": "/europe/static/js/2.879a476d.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "/europe/static/js/2.879a476d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6a9a2c91322d798b8f3",
+    "url": "/europe/static/js/main.56ae60fa.chunk.js"
+  },
+  {
+    "revision": "2f968b27128e71c8989b",
+    "url": "/europe/static/js/runtime-main.aa09fac5.js"
+  }
+]);
